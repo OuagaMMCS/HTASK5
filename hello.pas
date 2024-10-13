187d51477a8461fc25067ca80c227fd869fc1f1e
Maksim Levko <chepapguevara@gmail.com>
2024-10-13 13:14:21 +0300

    task10 file + readme file added

---
+++ hello.pas
@@ -0,0 +1,13 @@
+﻿{
+Программа спрашивает язык, на котором говорит пользователь
+и выводит приветствие на этом языке.
+}
+begin
+  var Lang := ReadString('На каком языке вы говорите?');
+  case Lang of
+    'Deutsch': Print('Hallo');
+    'English': Print('Hello');
+    'Русский': Print('Привет');
+  else Print('שלום');
+  end;
+end.
